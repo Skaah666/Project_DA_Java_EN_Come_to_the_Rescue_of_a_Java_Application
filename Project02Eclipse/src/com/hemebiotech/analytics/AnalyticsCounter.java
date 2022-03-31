ba1eca4245db172c34892e69262ea93e6ade5cfd
Alex Sfez <alexandre.sfez@gmail.com>
2022-03-31 09:27:30 +0200

    change : public static void main(String [] args) throws Exception
    (Warning:(17, 36) C-style array declaration of parameter 'args')

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -10,24 +10,21 @@ import java.io.FileWriter;
  */
 
 public class AnalyticsCounter {
-    private static int headacheCount = 0;
+    private static int headCount = 0;
     private static int rashCount = 0;
     private static int pupilCount = 0;
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String [] args) throws Exception {
         /* first get input */
         BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/src/com/hemebiotech/analytics/symptoms.txt"));
         String line = reader.readLine();
 
-        int i = 0;
-        int headCount = 0;
         while (line != null) {
-            i++;
             System.out.println("symptom from file: " + line);
             if (line.equals("headache")) {
                 headCount++;
                 System.out.println("number of headaches: " + headCount);
-            } else if (line.equals("rush")) {
+            } else if (line.equals("rash")) {
                 rashCount++;
             } else if (line.contains("pupils")) {
                 pupilCount++;
@@ -38,7 +35,7 @@ public class AnalyticsCounter {
 
         /* next generate output */
         FileWriter writer = new FileWriter("Project02Eclipse/src/com/hemebiotech/analytics/result.out");
-        writer.write("headache: " + headacheCount + "\n");
+        writer.write("headache: " + headCount + "\n");
         writer.write("rash: " + rashCount + "\n");
         writer.write("dialated pupils: " + pupilCount + "\n");
         writer.close();
